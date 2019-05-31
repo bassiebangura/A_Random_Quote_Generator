78422269e1fa5501c5a5413c8dd428fa44be055a
bassiebangura <devbassiebangura@gmail.com>
2019-05-30 21:54:16 -0400

    Add random quotes to page working.

--- js/script.js
+++ js/script.js
@@ -5,28 +5,55 @@ project 1 - A Random Quote Generator
 
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
+// Create the array of quote objects and name it `quotes`.
+let quotes = [
+  {
+    quote: "quote 1",
+    source: "quote1-src",
+    citation: "quote1-cit",
+    year: "quote1-year"
+  },
+  {
+    quote: "quote 2",
+    source: "quote2-src",
+    citation: "quote2-cit",
+    year: "quote2-year"
+  },
+  {
+    quote: "quote 3",
+    source: "quote3-src",
+    citation: "quote3-cit",
+    year: "quote3-year"
+  }
+];
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+// console.log(quotes[0].quote);
 
+const getRandomQuote = () => {
+  let randomNumber = Math.floor(Math.random() * (quotes.length - 1));
+  return quotes[randomNumber];
+};
 
+const printQuote = () => {
+  let randomQuote = getRandomQuote();
+  let quoteContainer = document.getElementById("quote-box");
+  let pageContainer = document.querySelector('body');
+  let innerHTMLContent = "";
+  innerHTMLContent +=
+    '<p class="quote">' +
+    randomQuote.quote +
+    '</p><p class="source">' +
+    randomQuote.source +
+    '<span class="citation">' +
+    randomQuote.citation +
+    '</span><span class="year">' +
+    randomQuote.year +
+    "</span></p>";
 
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
+  quoteContainer.innerHTML = innerHTMLContent;
+  pageContainer.style.backgroundColor =
+    pageContainer.style.backgroundColor == "green" ? "blue" : "green";
+};
 /***
   Create the `printQuote` function to: 
    - Call the `getRandomQuote` function and assign it to a variable.
@@ -40,9 +67,6 @@ project 1 - A Random Quote Generator
    - Set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
 
-
-
-
 /***
   When the "Show another quote" button is clicked, the event listener 
   below will be triggered, and it will call, or "invoke", the `printQuote` 
@@ -50,7 +74,8 @@ project 1 - A Random Quote Generator
   comment.
 ***/
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
+document
+  .getElementById("loadQuote")
+  .addEventListener("click", printQuote, false);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
